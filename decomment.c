21a14cadc29b7e6664e90a7a10c0f3f6d636bfee
Kcptonls <cmoretti@cs.princeton.edu>
2021-02-16 10:39:17 -0500

    added c file

--- decomment.c
+++ decomment.c
@@ -1,3 +1,5 @@
+/*  Name: Kevin Chen 
+    File: decomment.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -132,26 +134,29 @@ enum Statetype handleDoubleBack(char ch) {
 }
 
 /* 
-    main takes a standard file input and deletes the comments off. 
+    main takes a standard input and deletes the comments off and 
+    return to standard output. 
     It places a space in front of the comment. The line spacing 
     is preserved. If the comment does not close, an error statement 
-    is thrown into an error file with the line number of the starting
+    is thrown into standard with the line number of the starting
     comment. Returns EXIT_FAILURE if comment doesn't close. 
     Returns 0 otherwise.
 */
 int main() {
     int ch;
     int lineNum = 1;
-    int enteredline = 0;
+    int enteredLine = 0;
     enum Statetype state = START;
     ch = getchar();
     while (ch != EOF)
     {
+        /* two conditionals to keep track of line number and update
+            enteredLine when not in comment. */
         if (ch == '\n') {
             lineNum += 1;
         }
         if (state != COMMENT && state != STAR) {
-            enteredline = lineNum;
+            enteredLine = lineNum;
         }
         
         switch (state) {
@@ -184,7 +189,7 @@ int main() {
     }
     if (state == COMMENT || state == STAR) {
         fprintf(stderr, "Error: line %d: unterminated comment\n", 
-        enteredline);
+        enteredLine);
         return(EXIT_FAILURE);
     }
     else if (state == FORWARD) {
